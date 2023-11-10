4d3afe1280be8144f662c613af385d169c8fc969
DariaPressley <daria.mik@gmail.com>
2023-11-09 22:21:58 -0800

    updating server with additional prompts

--- Main/server.js
+++ Main/server.js
@@ -12,9 +12,76 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db database.`)
 );
 
+const init = async () => {
+  const { homepage } = await inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'homepage',
+        message: 'What would you like to do?',
+        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
+      },
+
+    ])
+  switch (homepage) {
+    case 'view all departments':
+      viewAllDept()
+      break;
+    case 'view all roles':
+      viewAllRoles()
+      break;
+    case 'view all employees':
+      viewAllEmployees()
+      break;
+    case 'add a department':
+      addDepartment()
+      break;
+    case 'add a role':
+      createRole()
+      break;
+    case 'add an employee':
+      addEmployee()
+      break;
+    case 'update an employee role':
+      updateEmployee()
+      break;
+  }
+}
+
+const viewAllDept = async () => {
+  const [data] = await db.promise().query("SELECT * FROM department")
+  console.table(data)
+  init()
+}
+
+const viewAllEmployees = async () => {
+  const [data] = await db.promise().query("SELECT * FROM employee")
+  console.table(data)
+  init()
+}
+
+const viewAllRoles = async () => {
+  const [data] = await db.promise().query("SELECT * FROM role")
+  console.table(data)
+  init()
+}
+
+const addDepartment = async () => {
+  const { name } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'name of new department'
+    }
+  ])
+  const [data] = await db.promise().query("INSERT INTO department set name = ?", name)
+  console.log('department added')
+  init()
+}
+
 const createRole = async () => {
   const [department] = await db.promise().query("SELECT id as value, name as name FROM department")
-  const response = await inquirer.prompt ([
+  const response = await inquirer.prompt([
     {
       type: 'input',
       name: 'title',
@@ -33,51 +100,39 @@ const createRole = async () => {
     }
   ])
   await db.promise().query("INSERT INTO role set ?", response);
-  console.log ('role added');
-  init ();
+  console.log('role added');
+  init();
 }
 
-const init = async () => {
-  const {homepage} = await inquirer
-    .prompt([
-      {
-        type: 'list',
-        name: 'homepage',
-        message: 'What would you like to do?',
-        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
-      },
-
-    ])
-    switch (homepage) {
-      case 'view all departments':
-        //viewAllDept function here
-        break; 
-      case 'view all employees':
-        viewAllEmployees ()
-        break; 
-      case 'add a department':
-        addDepartment ()
-        break; 
-    }
-}
-
-const viewAllEmployees = async () => {
-  const [data] = await db.promise().query("SELECT * FROM employee")
-  console.table(data)
-  init ()
-}
-
-const addDepartment = async () => {
-  const {name} = await inquirer.prompt ([
+const addEmployee = async () => {
+  const [employee] = await db.promise().query("SELECT id as value, title as title FROM role")
+  const response = await inquirer.prompt([
     {
       type: 'input',
-      name: 'name',
-      message: 'Name of new department'
-    }
+      name: 'first_name',
+      message: 'what is the first name?',
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: 'what is the last name?',
+    },
+    {
+      type: 'list',
+      name: 'role_id',
+      message: 'what is the role id',
+      choices: employee
+    },
+    {
+      type: 'input',
+      name: 'manager_id',
+      message: 'what is the manager id?',
+    },
   ])
-  const [data] = await db.promise ().query ("INSERT INTO department set name = ?", name)
-  console.log ('department added')
-  init ()
+  await db.promise().query("INSERT INTO employee set ?", response);
+  console.log('employee added');
+  init();
 }
 
-init ()
+init()
+
